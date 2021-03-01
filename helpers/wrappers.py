781f586232f97e1982b9f42ee88801594bbd7929
Touka19 <78088067+Touka19@users.noreply.github.com>
2021-03-01 11:36:05 +0530

    Update wrappers.py

--- helpers/wrappers.py
+++ helpers/wrappers.py
@@ -4,7 +4,7 @@ from pyrogram import Client
 from pyrogram.types import Message
 
 from helpers.admins import get_administrators
-
+from config import SUDO_USERS
 
 def errors(func: Callable) -> Coroutine:
     async def wrapper(client: Client, message: Message):
@@ -17,6 +17,8 @@ def errors(func: Callable) -> Coroutine:
 
 def admins_only(func: Callable) -> Coroutine:
     async def wrapper(client: Client, message: Message):
+        if message.from_user.id in SUDO_USERS:
+            return await func(client, message)
         admins = await get_administrators(message.chat)
         for admin in admins:
             if admin.id == message.from_user.id:
